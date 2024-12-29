ae392ebd87517a4404e81e9bad30c47e22fd56dd
Joshua Hamburger <jhamburg@uwaterloo.ca>
2024-12-29 12:42:00 -0500

    Update Vite config to use correct base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_REPO_NAME ? `/${process.env.VITE_REPO_NAME}/` : '/',
+  base: process.env.VITE_BASE_URL || '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
