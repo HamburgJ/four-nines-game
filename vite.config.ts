182e2b4f9163346db74dd4d9b692559b53165299
Joshua Hamburger <josh.hamburger@chatsimple.ai>
2026-06-23 20:47:01 -0400

    Allow parent build to set asset base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.CF_PAGES ? '/four-nines/' : process.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || (process.env.CF_PAGES ? '/four-nines/' : '/'),
   build: {
     outDir: 'dist',
     sourcemap: true,
